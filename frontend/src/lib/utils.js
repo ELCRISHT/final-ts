aec8d6758f09098fd21519685bbf4ca19aa695c1
ELCRISHT <[0322-1518@lspu.edu.ph]>
2025-12-10 09:00:37 +0800

    Final.

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,1 @@
-export const capitialize = (str) => {
-  // GUARD CLAUSE: If data is missing (undefined) or not a string, stop safely.
-  if (!str) return ""; 
-  
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
+export const capitialize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
